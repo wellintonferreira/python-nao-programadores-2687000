911ba334e44204b4bb79c9d57817caee98a2de61
Wellinton Ferreira <160875820+wellintonferreira@users.noreply.github.com>
2024-02-22 14:48:21 +0000

    Resolucao exercicio

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,13 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = 'Wellinton'
+nome_completo = "Wellinton Ferreira"
 # Declare variáveis de cada tipo numérico
-
+num_inteiro = 10
+num_fracionario = 10.01
+print (nome)
 
 # Declare uma variável do tipo booleano
+tipo_booleano = True
+print (tipo_booleano)
