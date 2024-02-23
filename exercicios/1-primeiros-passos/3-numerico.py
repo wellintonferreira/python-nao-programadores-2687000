ac1dbf030879765014fa7f8d4ab8c41706e85653
Wellinton Ferreira <160875820+wellintonferreira@users.noreply.github.com>
2024-02-23 14:10:37 +0000

    resolucao exercicios

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -1,11 +1,19 @@
 data_nascimento = '05-10-1976'
-idade_numerica = 46
+idade = 46
 altura = 1.74
 
+soma = 1 + 1
+print (soma)
 # Descubra o tipo de dado de cada variável acima
-
+#print("data_nascimento ", type(data_nascimento), "idade ", type(idade), "altura ", type(altura))
 
 # Realize uma operação entre dados do tipo string e inteiro
-
+print(data_nascimento*2)
+print(f"Wellinton estudou hoje {8 - 2} horas!")
 
 # Realize uma operação entre dados do tipo inteiro e float
+meses = 58
+vida = meses / 12
+anos = int(vida)
+print(vida)
+print(anos)
